e796c0476fb774227bbdf369d3c444477342243d
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2026-02-23 11:44:02 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function multiplyNode(node, count, deep) {
+   for (let i = 0, copy; i < count - 1; i++) {
+        copy = node.cloneNode(deep);
+        node.parentNode.insertBefore(copy, node);
+    }
+}
+for(let i=1;i<11;i++){
+if(window.innerWidth<400){
+multiplyNode(document.querySelector(`.pic${i}`), 11, true);
+}
+else { multiplyNode(document.querySelector(`.pic${i}`),Math.floor(window.innerWidth/50)-1,true);
+     };
+  };
